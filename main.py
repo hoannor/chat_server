4e017daa7d6b35a6ca8b65eb206ab0dfefd77dc2
hoannor <halong4399@gmail.com>
2024-09-24 22:17:41 +0700

    ver 2(co them private chat, chi su dung cau lenh /quit de co the out ra, chua the ctrl+C de co the shut down client nhanh)

--- main.py
+++ main.py
@@ -3,31 +3,42 @@ import threading
 
 
 client_in_server = []
+client_name_in_server = []
 
 
 def client_proc(client, addr):
     msg = "Your Username: "
     client.sendall(bytes(msg, "utf-8"))
     username = client.recv(1024).decode("utf-8")
+    username = username.rstrip('\n')
+    client_name_in_server.append(username)
+    print(username)
     print(f"Client: {client}\n Username: {username}\n connected to server!\n")
     msg = "You're connected!\n"
     client.sendall(bytes(msg, "utf-8"))
     client_in_server.append(client)
     while True:
+        # pass : cau lenh khong co tac dung gi chi de de trong cho ngoac lenh
+        # continue : dung de bo qua moi cau lenh ben duoi
         try:
             message = client.recv(1024).decode("utf-8")
             if not message:
                 break
-            if message.startswith("/private"):
+            if message.startswith("/private "):
                 parts = message.split(' ', 2)
                 if len(parts) >= 3:
                     recipient, private_message = parts[1], parts[2]
-                    recipient.sendall(bytes(private_message, "utf-8"))
+                    private_message = username + ": " + private_message
+                    try:
+                        index = client_name_in_server.index(recipient)
+                        client_in_server[index].sendall(bytes(private_message, "utf-8"))
+                    except ValueError:
+                        ErrMessage = "User you want to chat do not in the server\n"
+                        client.sendall(bytes(ErrMessage, "utf-8"))
 
             elif message == "/quit\n":
                 break
             else:
-                username = username.rstrip('\n')
                 message = username + ": " + message
                 print(message)
                 for recipient in client_in_server:
@@ -40,7 +51,7 @@ def client_proc(client, addr):
     # xoa client khi ngat ket noi
     client_in_server.remove(client)
     client.close()
-    message = f"{username} disconnected!"
+    message = f"{username} disconnected!\n"
 
     print(message)
     for recipient in client_in_server:
